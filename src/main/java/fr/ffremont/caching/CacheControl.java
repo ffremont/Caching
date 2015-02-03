0966d7fa839e4867f7c575ec24fed28fb4c9e5c2
Florent FREMONT <ff.fremont.florent@gmail.com>
2015-02-03 07:25:22 +0100

    cache par validation en cours

--- src/main/java/fr/ffremont/caching/CacheControl.java
+++ src/main/java/fr/ffremont/caching/CacheControl.java
@@ -8,9 +8,13 @@ package fr.ffremont.caching;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,29 +24,62 @@ import org.springframework.stereotype.Component;
 @Component
 @Path("cachecontrol")
 public class CacheControl {
+
+    @Context
+    private HttpServletRequest request;
+    
+    private final static Logger LOG = LoggerFactory.getLogger(CacheControl.class);
     
     @GET
-    @Path("use1day")
-    public Response use1day(){
+    @Path("use60sec")
+    public Response use60sec() {
         javax.ws.rs.core.CacheControl cache = new javax.ws.rs.core.CacheControl();
-        cache.setMaxAge(3600 * 24);
-        cache.setSMaxAge(3600 * 24);
+        cache.setMaxAge(60);
         cache.setPrivate(false);
-        
-        
-        return Response.ok("ok").cacheControl(cache).build();
+
+        return Response.ok("En cache pendant 60 sec").cacheControl(cache).build();
+    }
+
+    @GET
+    @Path("disablecache")
+    public Response disablecache() {
+        javax.ws.rs.core.CacheControl cache = new javax.ws.rs.core.CacheControl();
+        cache.setNoCache(true); // pour IE 
+        cache.setNoStore(true); // HTTP 1.1 demande juste no-store
+
+        return Response.ok("Pas de cache").cacheControl(cache).build();
+    }
+
+    @GET
+    @Path("nostore")
+    public Response nostore() {
+        javax.ws.rs.core.CacheControl cache = new javax.ws.rs.core.CacheControl();
+        cache.setNoStore(true);
+
+        return Response.ok("No store").cacheControl(cache).build();
+    }
+
+    @GET
+    @Path("nocache")
+    public Response nocache() {
+        javax.ws.rs.core.CacheControl cache = new javax.ws.rs.core.CacheControl();
+        cache.setNoCache(true);
+
+        return Response.ok("No cache").cacheControl(cache).build();
     }
     
     @GET
-    @Path("nocache")
-    public Response nocache(){
+    @Path("validationtps")
+    public Response validationTps() {
         javax.ws.rs.core.CacheControl cache = new javax.ws.rs.core.CacheControl();
-        cache.setMustRevalidate(true);
-        cache.setNoCache(true);
-        cache.setNoStore(true);
+        cache.setMaxAge(60);
         cache.setPrivate(false);
         
-        
-        return Response.ok("Pas de cache").cacheControl(cache).build();
+        if(request.getHeader("If-Modified-Since") != null){
+            // ma ressource est encore bonne
+            return Response.status(Response.Status.NOT_MODIFIED).build();            
+        }        
+
+        return Response.ok("Ma petite donnée").lastModified(Date.from(Instant.now())).cacheControl(cache).build();
     }
 }
